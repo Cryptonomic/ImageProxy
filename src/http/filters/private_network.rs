b644d3315dc6b6c808a52e15f4222f7462f6c277
Bhaskar Kishore <50350646+bhaskarkishore@users.noreply.github.com>
2021-07-14 19:37:04 +0530

    Removed dead code

--- src/http/filters/private_network.rs
+++ src/http/filters/private_network.rs
@@ -1,23 +1,9 @@
 use hyper::Uri;
 use log::{debug, error, warn};
-use serde::Deserialize;
-use std::net::Ipv4Addr;
 
 use crate::dns::DnsResolver;
 
 use super::UriFilter;
-
-#[derive(Deserialize, Clone)]
-pub struct PrivateNetworkFilterConfig {
-    pub allow_private_connections: bool,
-    pub allowed_ips: Vec<IpPortPair>,
-}
-
-#[derive(Deserialize, Clone)]
-pub struct IpPortPair {
-    pub destination: Ipv4Addr,
-    pub port: u16,
-}
 
 pub struct PrivateNetworkFilter {
     dns_resolver: Box<dyn DnsResolver + Send + Sync>,
