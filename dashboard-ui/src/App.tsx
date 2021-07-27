34bb8d763ecfd7c96cd98d0a3ae98ac2d3b61e96
Taaseen Ali <taaseen.ali89@gmail.com>
2021-07-27 01:53:09 -0400

    add cors and further updates

--- dashboard-ui/src/App.tsx
+++ dashboard-ui/src/App.tsx
@@ -1,8 +1,6 @@
 import Dashboard from "./components/Dashboard";
 import Navbar from "./components/Navbar";
 import Panel from "./components/Panel";
-import ReportTable from "./components/ReportTable";
-import ModerationTable from "./components/ModerationTable";
 import Metrics from "./components/Metrics";
 import Configuration from "./components/Configuration";
 import Info from "./components/Info";
@@ -11,29 +9,10 @@ function App() {
   return (
     <div className="flex flex-col h-full w-full bg-background">
       <Navbar />
-      <Dashboard
-        names={[
-          "Info",
-          "Metrics",
-          "User Reports",
-          "Moderation Reports",
-          "Configuration",
-        ]}
-      >
+      <Dashboard names={["Metrics", "Configuration"]}>
         <Panel>
           <Info />
-        </Panel>
-
-        <Panel>
           <Metrics />
-        </Panel>
-
-        <Panel>
-          <ReportTable />
-        </Panel>
-
-        <Panel>
-          <ModerationTable />
         </Panel>
 
         <Panel>
--- dashboard-ui/src/utils/ImageProxy.tsx
+++ dashboard-ui/src/utils/ImageProxy.tsx
@@ -1,71 +1,13 @@
 import config from "../config.json";
 import parsePrometheusTextFormat from "parse-prometheus-text-format";
-import {
-  DescribeResponse,
-  FetchResponse,
-  ImageProxyError,
-  ImageProxyServer,
-} from "nft-image-proxy";
 
 export interface BuildInfo {
   package_version: string;
   git_version: string;
 }
 
-const server: ImageProxyServer = {
-  url: config.proxyURL,
-  version: "1.0.0",
-  apikey: config.apikey,
-};
-
 export const getInfo = async (): Promise<BuildInfo> => {
   return (await fetch(`${config.proxyURL}/info`)).json();
-};
-
-export const getReports = async (): Promise<
-  DescribeResponse | ImageProxyError
-> => {
-  const init = {
-    method: "POST",
-    body: JSON.stringify({
-      jsonrpc: "1.0.0",
-      method: "img_proxy_describe_report",
-    }),
-    headers: {
-      apikey: config.apikey,
-    },
-  };
-  const res = (
-    await fetch(`${config.proxyURL}`, init).then((res) => res.json())
-  ).result;
-  return res;
-};
-
-export const getModerationReports = async (): Promise<
-  FetchResponse | ImageProxyError
-> => {
-  const init = {
-    method: "POST",
-    body: JSON.stringify({
-      jsonrpc: "1.0.0",
-      method: "img_proxy_describe",
-      params: {
-        urls: ["*"],
-      },
-    }),
-    headers: {
-      apikey: config.apikey,
-    },
-  };
-  const res = await fetch(`${config.proxyURL}`, init).then((res) =>
-    res
-      .json()
-      .then((json) =>
-        json.result.map((entry: any, i: number) => ({ ...entry, index: i }))
-      )
-  );
-
-  return res;
 };
 
 export const getMetrics = async () => {
