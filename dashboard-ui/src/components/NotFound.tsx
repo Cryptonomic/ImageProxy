6523f599d29672ff47e3ba94f8294a75ad6fa497
Taaseen Ali <taaseen.ali89@gmail.com>
2021-08-11 09:15:16 -0400

    add NotFound.tsx

---
+++ dashboard-ui/src/components/NotFound.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div className="">
+            <div>404 Not Found </div>
+        </div>
+    )
+}
+
+export default NotFound
