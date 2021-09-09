e0ee4f24eef71e86d44c69e34f32080571944288
Siddharth Vaderaa <svaderaa@gmail.com>
2021-09-09 13:01:25 +0200

    switched over to aws sdk

--- src/aws/messages.rs
+++ src/aws/messages.rs
@@ -1,6 +1,8 @@
 use std::collections::HashSet;
 
 use serde::Deserialize;
+use aws_sdk_rekognition::model::ModerationLabel;
+use aws_sdk_rekognition::output::DetectModerationLabelsOutput;
 
 use crate::moderation::ModerationCategories;
 
@@ -12,12 +14,45 @@ pub struct Label {
     pub ParentName: String,
 }
 
+impl From<ModerationLabel> for Label {
+    fn from(m: ModerationLabel) -> Self {
+        Self {
+            Confidence: m.confidence.unwrap_or(-1.0),
+            Name: m.name.unwrap_or("".into()),
+            ParentName: m.parent_name.unwrap_or("".into()),
+        }
+    }
+}
+
+
+
+
 #[derive(Deserialize)]
 #[allow(non_snake_case)]
 pub struct RekognitionResponse {
     pub ModerationLabels: Vec<Label>,
     pub ModerationModelVersion: String,
 }
+
+impl From<DetectModerationLabelsOutput> for RekognitionResponse {
+    fn from(d: DetectModerationLabelsOutput) -> Self {
+        Self {
+            ModerationLabels: d
+                .moderation_labels
+                .unwrap()
+                .into_iter()
+                .map(|m| {
+                    let l: Label = m.into();
+                    l
+                })
+                .collect(),
+            ModerationModelVersion: d.moderation_model_version.unwrap_or("".into()),
+        }
+    }
+}
+
+
+
 
 impl RekognitionResponse {
     pub fn get_labels(&self) -> Vec<ModerationCategories> {
--- src/aws/mod.rs
+++ src/aws/mod.rs
@@ -1,30 +1,21 @@
 mod messages;
-mod util;
 
 use async_trait::async_trait;
-use base64;
-use chrono::prelude::*;
-use hex;
-use hyper::{Body, Client, Method, Request};
-use hyper_tls::HttpsConnector;
 use log::{debug, error};
-use serde_json::json;
-use std::env;
 
 use messages::RekognitionResponse;
-use util::{get_signature_key, sign};
+
+use aws_sdk_rekognition::model::Image;
+use aws_sdk_rekognition::{Blob, Client as ClientRekognition, Region};
 
 use crate::{
     document::Document,
     moderation::{ModerationProvider, ModerationResponse, ModerationService, SupportedMimeTypes},
     rpc::error::Errors,
-    utils::sha256,
 };
 
 pub struct Rekognition {
     pub region: String,
-    pub access_key: String,
-    pub secret_key: String,
 }
 
 #[async_trait]
@@ -63,93 +54,21 @@ impl ModerationProvider for Rekognition {
 }
 
 impl Rekognition {
-    fn get_host(&self) -> String {
-        format!("rekognition.{}.amazonaws.com", self.region)
-    }
-
-    fn get_url(&self) -> String {
-        format!("https://{}", self.get_host())
-    }
-
-    pub async fn get_moderation_labels(
+       pub async fn get_moderation_labels(
         &self,
         bytes: &hyper::body::Bytes,
     ) -> Result<RekognitionResponse, Box<dyn std::error::Error + Send + Sync>> {
-        let amz_target = "RekognitionService.DetectModerationLabels";
-        let service = "rekognition";
-        let content_type = "application/x-amz-json-1.1";
-        let canonical_uri = "/";
-        let canonical_querystring = "";
-        let utc = Utc::now();
-        let amz_date = utc.format("%Y%m%dT%H%M%SZ").to_string();
-        let date_stamp = utc.format("%Y%m%d").to_string();
-        let canonical_headers = format!(
-            "content-type:{}\nhost:{}\nx-amz-date:{}\nx-amz-target:{}\n",
-            content_type,
-            self.get_host(),
-            amz_date,
-            amz_target
-        );
-        let signed_headers = "content-type;host;x-amz-date;x-amz-target";
-        let algorithm = "AWS4-HMAC-SHA256";
-        let credential_scope = format!("{}/{}/{}/aws4_request", date_stamp, self.region, service);
-        let request_dict = json!({
-            "Image": {
-                "Bytes": base64::encode(bytes.to_vec()),
-            },
-            "MinConfidence": 50.0,
-        });
-        let request_dict_encoded = request_dict.to_string();
-        let payload_hash = sha256(request_dict_encoded.as_bytes());
-        let canonical_request = format!(
-            "{}\n{}\n{}\n{}\n{}\n{}",
-            "POST",
-            canonical_uri,
-            canonical_querystring,
-            canonical_headers,
-            signed_headers,
-            payload_hash
-        );
-        //let canonical_request = format!("{}", canonical_request);
-        let string_to_sign = format!(
-            "{}\n{}\n{}\n{}",
-            algorithm,
-            amz_date,
-            credential_scope,
-            sha256(canonical_request.as_bytes())
-        );
-        let signing_key =
-            get_signature_key(&self.secret_key, &date_stamp[..], &self.region, service);
-        let signature = sign(&signing_key, string_to_sign.as_bytes());
-        let authorization_header = format!(
-            "{} Credential={}/{}, SignedHeaders={}, Signature={}",
-            algorithm,
-            self.access_key,
-            credential_scope,
-            signed_headers,
-            hex::encode(signature)
-        );
-        let req = Request::builder()
-            .method(Method::POST)
-            .uri(self.get_url())
-            .header("Content-Type", content_type)
-            .header("X-Amz-Date", amz_date)
-            .header("X-Amz-Target", amz_target)
-            .header("Authorization", authorization_header)
-            .body(Body::from(request_dict_encoded))?;
-        let https = HttpsConnector::new();
-        let client = Client::builder().build::<_, hyper::Body>(https);
-
-        match client.request(req).await {
-            Ok(response) => {
-                debug!("Rekognition response, status={}", response.status()); //TODO
-                match hyper::body::to_bytes(response.into_body()).await {
-                    Ok(bytes) => match serde_json::from_slice::<RekognitionResponse>(&bytes) {
-                        Ok(r) => Ok(r),
-                        Err(e) => Err(Box::new(e)),
-                    },
-                    Err(e) => Err(Box::new(e)),
-                }
+        let region = Region::new(self.region.clone());
+        let shared_config = aws_config::from_env().region(region).load().await;
+        let client = ClientRekognition::new(&shared_config);
+        let req = client.detect_moderation_labels();
+        let blob = Blob::new(bytes.as_ref()); //.to_vec());
+        let img = Image::builder().bytes(blob).build();
+        let response = req.image(img).send().await;
+        match response {
+            Ok(output) => {
+                let rekognition_repsonse: RekognitionResponse = output.into();
+                Ok(rekognition_repsonse)
             }
             Err(e) => Err(Box::new(e)),
         }
@@ -158,9 +77,6 @@ impl Rekognition {
     pub fn new(aws_region: &str) -> Result<Self, Box<dyn std::error::Error + Send + Sync>> {
         Ok(Rekognition {
             region: aws_region.to_string(),
-            access_key: env::var("AWS_ACCESS_KEY_ID").expect("AWS_ACCESS_KEY_ID key not set"),
-            secret_key: env::var("AWS_SECRET_ACCESS_KEY")
-                .expect("AWS_SECRET_ACCESS_KEY key not set"),
         })
     }
 }
