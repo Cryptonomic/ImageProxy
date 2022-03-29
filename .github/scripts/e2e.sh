4a18b6977bc148779ba98873eeb50609174a893c
bhaskarkishore <50350646+bhaskarkishore@users.noreply.github.com>
2022-03-29 17:22:57 +0530

    Fixed conditionals

--- .github/scripts/e2e.sh
+++ .github/scripts/e2e.sh
@@ -3,7 +3,7 @@
 CURR_DIR=`pwd`
 
 # Reconfigure proxy configuration
-sed -i 's/"host": "database"/"host": "localhost"/' ./proxy.conf 
+sed -i 's/"host": "database"/"host": "localhos221"/' ./proxy.conf 
 sed -i 's/expose/ports/' ./docker-compose.yml
 sed -i 's/5432/5432:5432/' ./docker-compose.yml
 
@@ -28,10 +28,15 @@ do
     status=$?
 done
 
-if [[ -d "/proc/${PID}" ]]
+if [[ ! -d "/proc/${PID}" ]]
+then
     echo "Proxy failed to start"
-    echo "E2E test failed"    
-    cat "log/proxy.log"
+    echo "E2E test failed"
+    if [[ -f "log/proxy.log" ]]
+    then
+        cat "log/proxy.log"
+    fi
+    docker-compose down
     exit 1
 fi
 
