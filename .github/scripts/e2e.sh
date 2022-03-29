ad18a3a4173242bf841a526be54bbf9f0abc2cf8
bhaskarkishore <50350646+bhaskarkishore@users.noreply.github.com>
2022-03-29 16:53:22 +0530

    Updated e2e test script

--- .github/scripts/e2e.sh
+++ .github/scripts/e2e.sh
@@ -2,26 +2,46 @@
 
 CURR_DIR=`pwd`
 
-# Start the database
-docker-compose up database -d
-
 # Reconfigure proxy configuration
-sed -i 's/"host": "database"/"host": "localhost_!"/' ./proxy.conf 
+sed -i 's/"host": "database"/"host": "localhost"/' ./proxy.conf 
+sed -i 's/expose/ports/' ./docker-compose.yml
+sed -i 's/5432/5432:5432/' ./docker-compose.yml
+
+
+# Start the database
+echo "Starting database..."
+docker-compose up database -d
+sleep 10
 
 # Run the proxy and capture its pid
-cargo run &&
+echo "Starting proxy..."
+cargo run &
 PID=$!
 
+status=1
+
+while [[ $status -ne 0 ]] && [[ -d "/proc/${PID}" ]]
+do   
+    sleep 5
+    echo "Waiting for proxy to start..."
+    curl -s http://localhost:3000/info
+    status=$?
+done
+
 # Run the npm e2e test
+echo "Starting e2e test..."
 cd lib/npm/
-npm run test || touch "$CURR_DIR/failed.out"
+npm run test || touch "${CURR_DIR}/failed.out"
 
 # Clean up
-cd "$CURR_DIR"
+cd "${CURR_DIR}"
 docker-compose down
 kill -9 $PID
 
 # Report result
 if [ -f "failed.out" ]; then  
+  echo "E2E test failed"
   exit 1
 fi
+
+echo "E2E test passed"
